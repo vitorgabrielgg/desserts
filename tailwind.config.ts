184783d6b8e016b99b74a0c787710fd9a7b5048e
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-04 10:04:01 -0300

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [],
+} satisfies Config;
