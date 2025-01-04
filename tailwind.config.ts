808553cd3296b74c1878f9e58baacf277625b80f
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-04 10:21:50 -0300

    style: configure the project colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,21 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      rose: {
+        50: "hsl(20, 50%, 98%)",
+        100: "hsl(13, 31%, 94%)",
+        300: "hsl(14, 25%, 72%)",
+        400: "hsl(7, 20%, 60%)",
+        500: "hsl(12, 20%, 44%)",
+        900: "hsl(14, 65%, 9%)",
+      },
+    },
     extend: {
-      colors: {},
+      colors: {
+        red_color: "hsl(14, 86%, 42%)",
+        green_color: "hsl(159, 69%, 38%)",
+      },
     },
   },
   plugins: [],
