a8b5b85ca0bc272b3e47a37e9eab03fbf4693f5a
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-08 10:52:34 -0300

    style: add responsiveness to the product list

--- src/components/product-list/ProductList/index.tsx
+++ src/components/product-list/ProductList/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import productData from "../../../../data.json";
 import { ProductCard } from "../ProductCard";
 
 export const ProductList = () => {
   return (
-    <ul className="mt-7 grid grid-cols-1 gap-5">
+    <ul className="mt-7 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5 xl:gap-6">
       {productData.map((product, i) => (
         <ProductCard key={i} {...product} />
       ))}
