9586ebbbee6b8432b4dedce882929b56f3a85e4b
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-08 19:10:21 -0300

    feat: add the quantity decrement icon

---
+++ src/components/icons/QuantityDecrementIcon/index.tsx
@@ -0,0 +1,18 @@
+export const QuantityDecrementIcon = () => {
+  return (
+    <div className="group h-5 w-5 hover:bg-white border border-white rounded-full transition-colors flex justify-center items-end">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="11"
+        height="11"
+        fill="none"
+        viewBox="0 0 10 10"
+      >
+        <path
+          className="fill-white group-hover:fill-red_color transition-colors"
+          d="M0 .375h10v1.25H0V.375Z"
+        />
+      </svg>
+    </div>
+  );
+};
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,1 +1,2 @@
 export * from "./QuantityIncrementIcon";
+export * from "./QuantityDecrementIcon";
