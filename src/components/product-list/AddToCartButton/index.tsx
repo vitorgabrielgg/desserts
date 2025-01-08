9d130b0057793b5e89a95fe8ece85a5bd1868244
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-08 11:03:12 -0300

    style: add hover to "add to cart" button

--- src/components/product-list/AddToCartButton/index.tsx
+++ src/components/product-list/AddToCartButton/index.tsx
@@ -3,10 +3,10 @@ import AddToCartIcon from "../../../../public/images/icon-add-to-cart.svg";
 
 export const AddToCartButton = () => {
   return (
-    <button className="bg-white h-12 w-44 px-10 absolute bottom-2 left-2/4 -translate-x-2/4 border border-rose-500 rounded-3xl">
+    <button className="group bg-white h-12 w-44 px-10 absolute bottom-2 left-2/4 -translate-x-2/4 border border-rose-500 hover:border-red_color rounded-3xl transition-colors">
       <div className="flex items-center justify-center gap-2">
         <Image src={AddToCartIcon} alt="Cart icon" />
-        <p className="text-rose-900 font-semibold whitespace-nowrap">
+        <p className="text-rose-900 group-hover:text-red_color font-semibold whitespace-nowrap transition-colors">
           Add to Cart
         </p>
       </div>
