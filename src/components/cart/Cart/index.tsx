d88db9093aadbaab140a131a3e69d9c74895dd07
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-10 10:52:11 -0300

    feat: show the quantity of the cart item list

--- src/components/cart/Cart/index.tsx
+++ src/components/cart/Cart/index.tsx
@@ -13,9 +13,20 @@ export const Cart = () => {
     0
   );
 
+  const cartItemListTotalQuantity = products.reduce(
+    (acc, cur) => acc + cur.quantity,
+    0
+  );
+
   return (
     <section className="bg-white px-6 py-6 shadow rounded-lg lg:max-w-[22rem] xl:max-w-96 flex-1">
-      <h2 className="text-red_color font-bold text-2xl">Your Cart (0)</h2>
+      <h2 className="text-red_color font-bold text-2xl">
+        Your Cart
+        <span aria-label="Cart item list total quantity">
+          {" "}
+          ({cartItemListTotalQuantity})
+        </span>
+      </h2>
 
       {!products.length ? (
         <EmptyCart />
