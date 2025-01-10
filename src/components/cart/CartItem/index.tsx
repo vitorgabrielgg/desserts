2c0f3168cfc1d722e0a21af41fa1508616e8f7f3
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-10 09:56:51 -0300

    feat: show the product in the cart item list

--- src/components/cart/CartItem/index.tsx
+++ src/components/cart/CartItem/index.tsx
@@ -12,11 +12,26 @@ export const CartItem = ({ name, price, quantity }: CartItemProps) => {
   return (
     <li className="flex items-center justify-between border-b border-rose-100 py-4">
       <div className="flex flex-col gap-1">
-        <h3 className="font-semibold text-rose-900">{name}</h3>
+        <h3
+          className="font-semibold text-rose-900"
+          aria-label={`Cart item: ${name}`}
+        >
+          {name}
+        </h3>
         <div className="flex gap-2.5">
-          <p className="font-semibold text-red_color">{quantity}x</p>
-          <p className="ml-2.5 text-rose-400">@ {priceFormatter(price)}</p>
-          <p className="font-semibold text-rose-500">
+          <p
+            className="font-semibold text-red_color"
+            aria-label={`Quantity of ${name}`}
+          >
+            {quantity}x
+          </p>
+          <p className="ml-2.5 text-rose-400" aria-label={`Price of ${name}`}>
+            @ {priceFormatter(price)}
+          </p>
+          <p
+            className="font-semibold text-rose-500"
+            aria-label={`Total price of ${name}`}
+          >
             {priceFormatter(quantity * price)}
           </p>
         </div>
