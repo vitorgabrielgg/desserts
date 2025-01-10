781159eb4dd51c3ba0cdb7d3108c4e6d35e5c260
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-10 09:51:15 -0300

    feat: add the cart item list component

--- src/components/cart/Cart/index.tsx
+++ src/components/cart/Cart/index.tsx
@@ -1,25 +1,27 @@
+import { useAppSelector } from "@/store";
 import { CarbonNeutral } from "../CarbonNeutral";
-import { CartItem } from "../CartItem";
+import { CartItemList } from "../CartItemList";
 import { ConfirmOrderButton } from "../ConfirmOrderButton";
 import { EmptyCart } from "../EmptyCart";
 import { OrderTotal } from "../OrderTotal";
 
 export const Cart = () => {
+  const { products } = useAppSelector((state) => state.cart);
+
   return (
     <section className="bg-white px-6 py-6 shadow rounded-lg lg:max-w-[22rem] xl:max-w-96 flex-1">
       <h2 className="text-red_color font-bold text-2xl">Your Cart (0)</h2>
 
-      <EmptyCart />
-
-      {/* <ul className="mt-2">
-        <CartItem name="Classic Tiramisu" price={5.5} quantity={1} />
-        <CartItem name="Vanilla Bean Crème Brûlée" price={7.0} quantity={4} />
-        <CartItem name="Vanilla Panna Cotta" price={6.5} quantity={2} />
-      </ul>
-
-      <OrderTotal totalPrice={46.5} />
-      <CarbonNeutral />
-      <ConfirmOrderButton /> */}
+      {!products.length ? (
+        <EmptyCart />
+      ) : (
+        <>
+          <CartItemList products={products} />
+          <OrderTotal totalPrice={46.5} />
+          <CarbonNeutral />
+          <ConfirmOrderButton />
+        </>
+      )}
     </section>
   );
 };
---
+++ src/components/cart/CartItemList/index.tsx
@@ -0,0 +1,21 @@
+import { CartItemProps } from "@/types";
+import { CartItem } from "../CartItem";
+
+interface CartListProps {
+  products: CartItemProps[];
+}
+
+export const CartItemList = ({ products }: CartListProps) => {
+  return (
+    <ul className="mt-2">
+      {products.map((product, i) => (
+        <CartItem
+          key={i}
+          name={product.name}
+          price={product.price}
+          quantity={product.quantity}
+        />
+      ))}
+    </ul>
+  );
+};
