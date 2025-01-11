8050d8d40fc9f46c1c7e2e58936e1e696d611ccf
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-11 11:05:52 -0300

    feat: add a function to update the isOpen state in modalStore to true

--- src/components/cart/ConfirmOrderButton/index.tsx
+++ src/components/cart/ConfirmOrderButton/index.tsx
@@ -1,6 +1,15 @@
+import { useAppDispatch } from "@/store";
+import { openModal } from "@/store/Modal/modal.store";
+
 export const ConfirmOrderButton = () => {
+  const dispatch = useAppDispatch();
+
   return (
-    <button className="bg-red_color hover:bg-dark_red_color w-full py-4 text-white font-semibold mt-6 rounded-full transition-colors">
+    <button
+      className="bg-red_color hover:bg-dark_red_color w-full py-4 text-white font-semibold mt-6 rounded-full transition-colors"
+      aria-label="Confirm order button"
+      onClick={() => dispatch(openModal())}
+    >
       Confirm Order
     </button>
   );
