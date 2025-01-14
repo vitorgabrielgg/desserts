a0a793c3393397bb4b1783da7d13415c92f369fa
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-01-14 10:38:15 -0300

    feat: add a function to reset the global state of the cart

--- src/components/modal/StartNewOrderButton/index.tsx
+++ src/components/modal/StartNewOrderButton/index.tsx
@@ -1,6 +1,22 @@
 import { Button } from "@/components/ui";
+import { useAppDispatch } from "@/store";
+import { resetCart } from "@/store/Cart/cart.store";
+import { closeModal } from "@/store/Modal/modal.store";
 import React from "react";
 
 export const StartNewOrderButton = () => {
-  return <Button text="Start New Order" />;
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(resetCart());
+    dispatch(closeModal());
+  };
+
+  return (
+    <Button
+      text="Start New Order"
+      aria-label="Start new order button"
+      onClick={handleClick}
+    />
+  );
 };
